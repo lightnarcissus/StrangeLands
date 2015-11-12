8c7b5f5ade5c0197d734fd5db97bf5b74535872e
Ansh <lightnarcissus@gmail.com>
2015-11-12 08:17:11 -0500

    Added a controller manager

---
+++ StrangeLands/Assets/CharacterManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterManager : MonoBehaviour {
+
+	public List<GameObject> controllers;
+	private int controllerCount;
+	public GameObject activeController;
+	private int randController;
+	// Use this for initialization
+	void Start () {
+		controllerCount = controllers.Count;
+		for (int i=1; i<controllerCount; i++) {
+			controllers[i].SetActive (false);
+		}
+		activeController = controllers [0];
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Random.value < 0.005) {
+
+			randController=Random.Range (0,2);
+			if(controllers[randController]!=activeController)
+			{
+				Debug.Log ("change active");
+				activeController.SetActive (false);
+				activeController=controllers[randController];
+				activeController.SetActive (true);
+			}
+
+		}
+	
+	}
+}
--- StrangeLands/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ StrangeLands/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -71,7 +71,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
             {
                 StartCoroutine(m_JumpBob.DoBobCycle());
-                PlayLandingSound();
+                //PlayLandingSound();
                 m_MoveDir.y = 0f;
                 m_Jumping = false;
             }
@@ -116,7 +116,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
                 if (m_Jump)
                 {
                     m_MoveDir.y = m_JumpSpeed;
-                    PlayJumpSound();
+                    //PlayJumpSound();
                     m_Jump = false;
                     m_Jumping = true;
                 }
@@ -167,11 +167,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
             // pick & play a random footstep sound from the array,
             // excluding sound at index 0
             int n = Random.Range(1, m_FootstepSounds.Length);
-            m_AudioSource.clip = m_FootstepSounds[n];
-            m_AudioSource.PlayOneShot(m_AudioSource.clip);
-            // move picked sound to index 0 so it's not picked next time
-            m_FootstepSounds[n] = m_FootstepSounds[0];
-            m_FootstepSounds[0] = m_AudioSource.clip;
+			if (m_FootstepSounds.Length > 0) {
+				m_AudioSource.clip = m_FootstepSounds [n];
+				m_AudioSource.PlayOneShot (m_AudioSource.clip);
+				// move picked sound to index 0 so it's not picked next time
+				m_FootstepSounds [n] = m_FootstepSounds [0];
+				m_FootstepSounds [0] = m_AudioSource.clip;
+			}
         }
 
 
